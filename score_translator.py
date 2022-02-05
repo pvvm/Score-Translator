39b33a024372027d3271662dde549e403c88b175
Pedro <pv.mizuno@gmail.com>
2022-02-05 17:06:05 -0300

    Print do alinhamento

--- score_translator.py
+++ score_translator.py
@@ -5,6 +5,8 @@ def translator(file):
     score = 0
     best_score = 0
     gap = 0
+    best_align = []
+    align = []
 
     for line in file:
 
@@ -16,15 +18,19 @@ def translator(file):
             if best_score < score:
                 #print(seqA, seqB, score)
                 best_score = score
+                best_align = align
             score = 0
+            align = []
 
         # First sequence
         if line[0] == "A":
             seqA = (line[:-1])[3:]
+            align.append(line[:-1])
 
         # Second sequence
         elif line[0] == "B":
             seqB = (line[:-1])[3:]
+            align.append(line)
 
             # Compare each nucleobase between sequences
             for i in range(len(seqA)):
@@ -51,14 +57,22 @@ def translator(file):
                 
                 #print(seqA[i], seqB[i], score, gap)
 
+    for line in best_align:
+        print(line)
+
     return best_score    
 
 def main():
-    command = "run-mummer3 " + str(sys.argv[1]) + " " + str(sys.argv[2]) + " " + str(sys.argv[3])
-    os.system(command)
-    
-    print("\n\nFinding and translating the best score...\n")
-    file_name = (str(sys.argv[3]) + ".align")
+    if str(sys.argv[1]) == "-m":
+        command = "run-mummer3 " + str(sys.argv[2]) + " " + str(sys.argv[3]) + " " + str(sys.argv[4])
+        os.system(command)
+        
+        file_name = (str(sys.argv[4]) + ".align")
+
+    elif str(sys.argv[1] == "-p"):
+        file_name = str(sys.argv[2])
+
+    print("\n\nFinding and translating the best alignment score...\n")    
     f = open(file_name, "r")
     score = translator(f)
     print('Translated score:', score, "\n\n")
